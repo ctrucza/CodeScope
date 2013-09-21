918c35a1a47890c7578fdb18dc385d5134a49cce
Csaba Trucza <csaba.trucza@iquestgroup.com>
2013-09-21 16:00:36 +0300

    Added CommandLine class, unit tests for it

---
+++ src/codescope.Tests/CommandLineTests.cs
@@ -0,0 +1,76 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace codescope.Tests
+{
+    [TestFixture]
+    public class CommandLineTests
+    {
+        [Test]
+        public void CommandLine_WithOneParameter_HasOneParameter()
+        {
+            CommandLine cmd = new CommandLine(new []{"parameter"});
+            CollectionAssert.Contains(cmd.GetParameters(), "parameter");
+        }
+
+        [Test]
+        public void CommandLine_WithTwoParameters_HasTwoParameters()
+        {
+            CommandLine cmd = new CommandLine(new []{"firstParameter", "secondParameter"});
+            Assert.AreEqual(2, cmd.GetParameters().Count());
+            CollectionAssert.Contains(cmd.GetParameters(), "firstParameter");
+            CollectionAssert.Contains(cmd.GetParameters(), "secondParameter");
+        }
+
+        [Test]
+        public void CommandLine_WithOneSwitch_HasNoParameters()
+        {
+            CommandLine cmd = new CommandLine(new []{"-a"});
+            Assert.AreEqual(0, cmd.GetParameters().Count());
+        }
+
+        [Test]
+        public void CommandLine_WithOneSwitch_HasTheSwitchOn()
+        {
+            CommandLine cmd = new CommandLine(new[]{"-a"});
+            Assert.IsTrue(cmd.GetSwitch("a"));
+        }
+
+        [Test]
+        public void CommandLine_WithoutASwitch_HasTheSwitchOff()
+        {
+            CommandLine cmd = new CommandLine(new string[]{});
+            Assert.IsFalse(cmd.GetSwitch("a"));
+        }
+
+        [Test]
+        public void CommandLine_WithTwoSwitches_HasBothSwitchedOn()
+        {
+            CommandLine cmd = new CommandLine(new []{"-a", "-b"});
+            Assert.IsTrue(cmd.GetSwitch("a"));
+            Assert.IsTrue(cmd.GetSwitch("b"));
+        }
+
+
+        [Test]
+        public void CommandLine_WithOneOption_HasTheOptionSet()
+        {
+            CommandLine cmd = new CommandLine(new []{"-option", "value"});
+            Assert.AreEqual("value", cmd.GetOption("option"));
+        }
+
+        [Test]
+        public void CommandLine_WithNoOptions_HasTheOptionEmpty()
+        {
+            CommandLine cmd = new CommandLine(new string[]{});
+            Assert.AreEqual("", cmd.GetOption("option"));
+        }
+
+        [Test]
+        public void CommandLine_WithAnOption_ConsumesTheOption()
+        {
+            CommandLine cmd = new CommandLine(new []{"-option", "value"});
+            CollectionAssert.DoesNotContain(cmd.GetParameters(), "value");
+        }
+    }
+}
---
+++ src/codescope/CommandLine.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace codescope
+{
+    public class CommandLine
+    {
+        private readonly List<string> parameters = new List<string>();
+        private readonly List<string> switches = new List<string>();
+        private readonly Dictionary<string, string> options = new Dictionary<string, string>();
+
+        private readonly string[] args;
+        private int index;
+
+        public CommandLine(string[] args)
+        {
+            this.args = args;
+            Parse();
+        }
+
+        private void Parse()
+        {
+            index = 0;
+            while (index < args.Count())
+            {
+                ParseNext();
+            }
+        }
+
+        private void ParseNext()
+        {
+            string name = GetNext();
+
+            if (IsParameter(name))
+            {
+                parameters.Add(name);
+                return;
+            }
+
+            if (IsSwitch())
+            {
+                switches.Add(name);
+                return;
+            }
+
+            string value = GetNext();
+            options[name] = value;
+        }
+
+        private bool IsSwitch()
+        {
+            string next = PeekNext();
+            return (NoMoreArguments() || IsOption(next));
+        }
+
+        private static bool IsParameter(string name)
+        {
+            return !IsOption(name);
+        }
+
+        private static bool IsOption(string name)
+        {
+            return name.StartsWith("-");
+        }
+
+        private string PeekNext()
+        {
+            if (NoMoreArguments())
+            {
+                return "";
+            }
+
+            return args[index];
+        }
+
+        private bool NoMoreArguments()
+        {
+            return index >= args.Count();
+        }
+
+        private string GetNext()
+        {
+            string result = args[index];
+            index++;
+            return result;
+        }
+
+        public IEnumerable<string> GetParameters()
+        {
+            return parameters;
+        }
+
+        public bool GetSwitch(string switchName)
+        {
+            return switches.Contains(GetOptionName(switchName));
+        }
+
+        private static string GetOptionName(string name)
+        {
+            return "-" + name;
+        }
+
+        public string GetOption(string optionName)
+        {
+            string key = GetOptionName(optionName);
+            return options.ContainsKey(key) ? options[key] : "";
+        }
+    }
+}
