42ac31a3ecd94746a9981cd98391bbfeaac54965
Csaba Trucza <csaba.trucza@iquestgroup.com>
2015-01-05 16:45:14 +0200

    POC: counting classes
    the most straightforward approach

---
+++ src/poc/CountClasses/CountClasses.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.MSBuild;
+
+namespace CountClasses
+{
+    public class CountClasses
+    {
+        static int  Main(string[] arguments)
+        {
+            var workspace = MSBuildWorkspace.Create();
+
+            string solutionFileName = arguments[0];
+            var solution = workspace.OpenSolutionAsync(solutionFileName).Result;
+            foreach (var project in solution.Projects)
+            {
+                foreach (var document in project.Documents)
+                {
+                    var root = document.GetSyntaxRootAsync().Result;
+                    var classes = root.DescendantNodes().OfType<ClassDeclarationSyntax>();
+                    foreach (var classDeclarationSyntax in classes)
+                    {
+                        Console.WriteLine(classDeclarationSyntax.Identifier);
+                    }
+                }
+            }
+
+            return 0;
+        }
+    }
+}
