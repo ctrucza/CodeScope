bb7bd7b9906d9eba8b2cc348c73a6e62771e163c
Csaba Trucza <csaba.trucza@iquestgroup.com>
2015-06-12 15:46:26 +0300

    Added new project for class analysis

---
+++ src/poc/HackingClasses/HackingClassesTests/Tests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using NUnit.Framework;
+
+namespace HackingClassesTests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void test_usage()
+        {
+            string source = "class Foo {}";
+            var syntaxTree = CSharpSyntaxTree.ParseText(source);
+            var classDeclarationSyntax = syntaxTree.GetRoot().DescendantNodes().OfType<ClassDeclarationSyntax>().Single();
+
+            Class c = new Class(classDeclarationSyntax);
+            
+            Assert.AreEqual("Foo", c.Name());
+        }
+    }
+
+    public class Class
+    {
+        public Class(ClassDeclarationSyntax classDeclarationSyntax)
+        {
+        }
+
+        public string Name()
+        {
+            return "Foo";
+        }
+    }
+}
---
+++ src/poc/HackingClasses/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.MSBuild;
+
+namespace HackingClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string solutionFileName = args[0];
+
+            var solution = OpenSolution(solutionFileName);
+
+            foreach (var project in solution.Projects)
+            {
+            }
+        }
+
+        private static Solution OpenSolution(string solutionFileName)
+        {
+            var workspace = MSBuildWorkspace.Create();
+            var solution = workspace.OpenSolutionAsync(solutionFileName).Result;
+            return solution;
+        }
+    }
+}
